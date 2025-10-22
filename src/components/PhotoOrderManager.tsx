d9418b12d037b5b6f1d78fc301d7aaeedb1f1f32
[dyad] <robsonrmaia@hotmail.com>
2025-10-22 13:37:43 -0300

    [dyad] Corrigido erro de sintaxe JSX no PhotoOrderManager - wrote 1 file(s)

--- src/components/PhotoOrderManager.tsx
+++ src/components/PhotoOrderManager.tsx
@@ -222,7 +222,7 @@ export function PhotoOrderManager({
                   </div>
                 )}
               </Droppable>
-            </div>
+            </DragDropContext>
           </CardContent>
         </Card>
   );
